fef852cbf873d14e1380f6317f334e1be592e8e9
MrzBldk <anton_lityagin@mail.ru>
2022-12-05 08:33:13 +0300

    small changes

--- src/app/Navbar.jsx
+++ src/app/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import './Navbar.css'
+import styles from './Navbar.module.css'
 
 export const Navbar = () => {
     return (
         <nav>
             <section>
                 <h1>PokeAPI Client</h1>
-                <div className="navContent">
-                    <div className="navLinks">
+                <div className={styles.navContent}>
+                    <div className={styles.navLinks}>
                         <Link to="/">Pokemons</Link>
                         <Link to="/owned">My Pokemons</Link>
                     </div>
